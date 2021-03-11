2f0e452239777feaf8ca84e127b884a9fba63a47
pwilson2021 <philip.yirenkyi@turntabl.io>
2021-03-11 19:14:05 +0000

    setup gradle project

---
+++ src/main/java/turntabl/io/order_validation_service/OrderValidationServiceApplication.java
@@ -0,0 +1,13 @@
+package turntabl.io.order_validation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderValidationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderValidationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/turntabl/io/order_validation_service/OrderValidationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package turntabl.io.order_validation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderValidationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
